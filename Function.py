a804b9b25a69649156f9e07a5fc65656773ba217
AJEET JAISWAL <AJEET.JAISWAL922@GMAIL.COM>
2018-04-17 23:20:01 +0530

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,6 @@
+def Divisibility(a,b):
+    if b%a == 0:
+        print("b is divisible by a")
+    else:
+        print("b is not divisible by a")
+Divisibility(2,4)
---
+++ Ifelse.py
@@ -0,0 +1,8 @@
+a=3
+b=5
+if b % a == 0:
+    print("b is divisible by a")
+elif b+1 == 10:
+    print("increement in b produce 10")
+else:
+    print("you are in else")
